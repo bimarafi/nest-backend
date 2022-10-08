cc41c74a0ec9b2d145a6e209e22d852d3dd12d96
Bima Sukma Wijaya <bimasukmawijaya@gmail.com>
2022-10-08 11:00:37 +0700

    init

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ModuleModule } from './module/module.module';
+import { ModuleModule } from './module/module.module';
+
+@Module({
+  imports: [ModuleModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
